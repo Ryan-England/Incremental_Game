0690e9ebe63008fb1a03ad0a16fb5dd7ef54616a
Ryan England <raenglandwork@gmail.com>
2024-10-15 10:34:38 -0700

    Added button code for Step 1.

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("h2");
+button.innerHTML = "<button>🧶</button>";
+app.append(button);
