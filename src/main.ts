c46e7df57731de857c88ddb9b6fe046356901301
Ryan England <raenglandwork@gmail.com>
2024-10-15 11:00:18 -0700

    Fixed div element to correctly use template literals. Step 2 complete.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-let counter:number = 0;
+let counter: number = 0;
 
 const gameName = "Crochet_Creations";
 document.title = gameName;
@@ -11,14 +11,14 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const score = document.createElement('div');
-score.innerText = "Yarn by yard: ${counter}" + counter;
+const score = document.createElement("div");
+score.innerText = `Yarn by yard: ${counter}`;
 app.append(score);
 
 const button = document.createElement("button");
 button.textContent = "🧶🧶🧶";
-button.addEventListener('click', () => {
-    counter++;
+button.addEventListener("click", () => {
+  counter++;
 });
 
 app.append(button);
