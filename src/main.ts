17f5c393db6ae0796798930b9764dc2d8f76c5c3
Ryan England <raenglandwork@gmail.com>
2024-10-15 10:47:46 -0700

    Updated button text. Step 1 complete.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.textContent = "<button type='button'>🧶</button>";
+const button = document.createElement("button");
+button.textContent = "🧶🧶🧶";
+
 app.append(button);
