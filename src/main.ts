41aee4ce59caa79eab5c8b37af614830dc3922d5
Ryan England <raenglandwork@gmail.com>
2024-10-17 20:41:32 -0700

    Removed deprecated code comments. All Steps now complete.

--- src/main.ts
+++ src/main.ts
@@ -35,21 +35,21 @@ const availableItems: Item[] = [
     description: `Crochet a Hat to keep off the sun`,
     price: 10,
     rateIncrease: 0.0001,
-    counter: 0
+    counter: 0,
   },
   {
     name: `Crochet Sweater`,
     description: `Crochet a sweater to look snazzy`,
     price: 30,
     rateIncrease: 0.0009,
-    counter: 0
+    counter: 0,
   },
   {
     name: "Crochet Table",
     description: `Crochet a table to crochet upon`,
     price: 100,
     rateIncrease: 0.002,
-    counter: 0
+    counter: 0,
   },
   {
     name: "Crochet Dragon",
@@ -64,7 +64,7 @@ const availableItems: Item[] = [
     price: 10000,
     rateIncrease: 0.4,
     counter: 0,
-  }
+  },
 ];
 
 const buttons: HTMLButtonElement[] = [];
@@ -86,31 +86,6 @@ function updateScore() {
       }
     }
   }
-
-  /*
-  if (hatCounter != 0) {
-    score.innerText += `
-    Hats crafted: ${hatCounter}`;
-  }
-  if (tableCounter != 0) {
-    score.innerText += `
-    Tables made: ${tableCounter}`;
-  }
-  if (dragonCounter != 0) {
-    score.innerText += `
-    Dragons created: ${dragonCounter}`;
-  }
-
-  if (yarnCounter >= 10) {
-    hatUpgrade.disabled = false;
-  }
-  if (yarnCounter >= 100) {
-    tableUpgrade.disabled = false;
-  }
-  if (yarnCounter >= 1000) {
-    dragonUpgrade.disabled = false;
-  }
-  */
 }
 
 const button = document.createElement("button");
@@ -138,60 +113,6 @@ for (const item of availableItems) {
   buttons.push(itemButton);
   itemButton.disabled = true;
 }
-
-/*
-const hatUpgrade = document.createElement("button");
-hatUpgrade.textContent = "Crochet a Hat to keep off the sun";
-let hatUpgradePrice: number = 10;
-hatUpgrade.addEventListener("click", () => {
-  if (yarnCounter >= hatUpgradePrice) {
-    yarnCounter -= hatUpgradePrice;
-    hatUpgradePrice *= 1.15;
-    hatCounter++;
-    yarnPerMs += 0.0001;
-    hatUpgrade.textContent = `Crochet a Hat (cost: ${numberFormat.format(hatUpgradePrice)} yards)`;
-  }
-});
-app.append(hatUpgrade);
-hatUpgrade.disabled = true;
-
-const tableUpgrade = document.createElement("button");
-tableUpgrade.textContent = "Crochet a table to crochet upon";
-let tableUpgradePrice: number = 100;
-tableUpgrade.addEventListener("click", () => {
-  if (yarnCounter >= tableUpgradePrice) {
-    yarnCounter -= tableUpgradePrice;
-    tableUpgradePrice *= 1.15;
-    tableCounter++;
-    yarnPerMs += 0.002;
-    tableUpgrade.textContent = `Crochet a Table (cost: ${numberFormat.format(tableUpgradePrice)} yards)`;
-  }
-});
-app.append(tableUpgrade);
-tableUpgrade.disabled = true;
-
-const dragonUpgrade = document.createElement("button");
-dragonUpgrade.textContent = "Crochet a cute dragon to fetch more yarn";
-let dragonUpgradePrice: number = 1000;
-dragonUpgrade.addEventListener("click", () => {
-  if (yarnCounter >= dragonUpgradePrice) {
-    yarnCounter -= dragonUpgradePrice;
-    dragonUpgradePrice *= 1.15;
-    dragonCounter++;
-    yarnPerMs += 0.05;
-    dragonUpgrade.textContent = `Crochet a Dragon (cost: ${numberFormat.format(dragonUpgradePrice)} yards)`;
-  }
-});
-app.append(dragonUpgrade);
-dragonUpgrade.disabled = true;
-*/
-
-/*
-setInterval(() => {
-  counter++;
-  score.innerText = `Yarn by yards: ${counter}`;
-}, 1000);
-*/
 
 let startOfFrame: DOMHighResTimeStamp;
 function step(timestamp: DOMHighResTimeStamp) {
